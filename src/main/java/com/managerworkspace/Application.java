af4094fc4bcb82e582424040ceab43786863f6c4
zhibareva <zhibareva.a@gmail.com>
2022-12-05 09:31:49 +0300

    application class

---
+++ src/main/java/com/managerworkspace/Application.java
@@ -0,0 +1,12 @@
+package com.managerworkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    new SpringApplication(Application.class).run(args);
+  }
+}
